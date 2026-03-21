edc9e57b1a5d71e0420e26579371518d58e3f8ce
Codig-cpu <aarushdixit604@gmail.com>
2026-03-21 17:43:05 +0530

    New Folder

---
+++ List/Activity1.py
@@ -0,0 +1,16 @@
+#Create an empty list 
+empty_list = []
+print(empty_list)
+
+#A list of different data types
+numbers = ['a' , 2, 'hello' , 4.7 , 5]
+print(numbers)
+
+# Use * operator
+triples = [1, 2, 3] * 3
+print(triples)
+
+#reverse the given list 
+aList = [100, 200, 300, 400, 500]
+aList = aList[::-1]
+print(aList, "\n")
