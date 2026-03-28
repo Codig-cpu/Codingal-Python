763b6193e29ebf8b09a299f467a439ba7e87cd09
Codig-cpu <aarushdixit604@gmail.com>
2026-03-28 17:24:29 +0530

    Set Operation

---
+++ Sets and Arrays/Set_Operation.py
@@ -0,0 +1,18 @@
+my_set = {1, 2, 3}
+print(my_set)
+
+my_set = {1.0, "Hello" , (1, 2, 3)}
+print(my_set)
+
+my_set1 = {1, 2, 3, 4, 5, 6, 8, 2 , 4, 5}
+print(my_set1)
+
+my_set = set([1, 2, 3, 2])
+print(my_set, "\n")
+
+num_set = set([7, 1, 3, 4, 5])
+print("Original set:")
+print(num_set)
+num_set.pop()
+print("After removing the first element from said set:")
+print(num_set, "\n")
