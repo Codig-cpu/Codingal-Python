d310e012ece4f727877d68571989a35a03064d10
Codig-cpu <aarushdixit604@gmail.com>
2026-05-23 17:34:44 +0530

    activity5

---
+++ Abstraction and Polymorphism/Class-Animal.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    def move(self):
+        pass
+
+class Human(Animal):
+
+    def move(self):
+        print("I can walk and run")
+
+class Snake(Animal):
+    def move(self):
+      print('I can crawl')
+
+class dog(Animal):
+    def move(self):
+        print("I can bark")
+
+R = Human()
+R.move()
+
+K = Snake()
+K.move()
+
+R = dog()
+R.move()
